247b0d747b2e12963edd163fa0a0627f0d22220d
wmakeouthill <wesley.correia@faculdadegran.edu.br>
2026-02-15 07:52:16 -0300

    sv

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: '/api',
-    googleClientId: '699875180084-3nlr1k3jf4kt0dlcqk0gs4qj875tq21a.apps.googleusercontent.com'
+  production: true,
+  apiUrl: '/api',
+  googleClientId: '105501776132-75qp7j87btik9423dk3eus1ueu4au5il.apps.googleusercontent.com',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: '/api',
-  googleClientId: '699875180084-3nlr1k3jf4kt0dlcqk0gs4qj875tq21a.apps.googleusercontent.com'
+  googleClientId: '105501776132-75qp7j87btik9423dk3eus1ueu4au5il.apps.googleusercontent.com',
 };
